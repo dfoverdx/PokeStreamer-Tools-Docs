587ea6b22c02cfab811fd9a121a64cd171e18012
Beth Hitch <dfoverdx@users.noreply.github.com>
2022-01-15 10:09:00 -0800

    part 2

--- js/deprecated.js
+++ js/deprecated.js
@@ -6,3 +6,25 @@ if (dismissed) {
   deprecated.alert();
   deprecated.on('close.bs.alert', () => localStorage.setItem('dismissed', 'true'));
 }
+
+const accepted = Boolean(localStorage.getItem('accepted') || false);
+if (!accepted) {
+  const modal = $('#dep-modal').modal({
+    show: true,
+    keyboard: false,
+    focus: true,
+  });
+  
+  const input = $('#deprecation-input');
+  const acceptBtn = $('#accept-deprecration-warning');
+  
+  input.on('change', () => acceptBtn.prop(
+    'disabled',
+    input.value() !== 'Because I understand that Pokemon Soul.link is NO LONGER SUPPORTED, I will use Pokelink instead! 🧐'
+  ));
+  
+  acceptBtn.on('click', () => {
+    localStorage.setItem('accepted' , 'true');
+    modal.hide();
+  });
+}
